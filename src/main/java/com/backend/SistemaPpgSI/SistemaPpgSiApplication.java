a89d50fcf637d7e64953754e63898e1a00df7d34
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-10-06 20:06:56 -0300

    Add files via upload

---
+++ src/main/java/com/backend/SistemaPpgSI/SistemaPpgSiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.SistemaPpgSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPpgSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPpgSiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/SistemaPpgSI/SistemaPpgSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.SistemaPpgSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPpgSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
